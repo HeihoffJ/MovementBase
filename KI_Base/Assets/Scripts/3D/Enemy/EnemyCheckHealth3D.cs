eb2aa18a9b64ff245e26132048fc6708eb02de1c
Jan <HeihoffJan@gmail.com>
2020-05-05 14:03:51 +0200

    unity musst noch geschlossen werden

---
+++ KI_Base/Assets/Scripts/3D/Enemy/EnemyCheckHealth3D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCheckHealth3D : MonoBehaviour
+{
+    Enemy enemy;
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+   public bool CheckHealth()
+    {
+        if (enemy.health <= enemy.health * enemy.retreatHealth / 100)
+            return true;
+        else
+            return false;
+    }
+}
